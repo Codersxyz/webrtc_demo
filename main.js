4bf7421cf545cd1a90b40633404d893858be49da
Suyog Burkule <95428654+Codersxyz@users.noreply.github.com>
2024-09-24 23:39:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,64 +1,67 @@
-let peerConnection = new RTCPeerConnection();
+let peerConnection = new RTCPeerConnection()
 let localStream;
 let remoteStream;
 
-const init = async () => {
-    localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
-    remoteStream = new MediaStream();
-    document.getElementById('user-1').srcObject = localStream;
-    document.getElementById('user-2').srcObject = remoteStream;
+let init = async () => {
+    localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
+    remoteStream = new MediaStream()
+    document.getElementById('user-1').srcObject = localStream
+    document.getElementById('user-2').srcObject = remoteStream
 
-    localStream.getTracks().forEach(track => {
+    localStream.getTracks().forEach((track) => {
         peerConnection.addTrack(track, localStream);
     });
 
-    peerConnection.ontrack = event => {
-        event.streams[0].getTracks().forEach(track => {
-            remoteStream.addTrack(track);
+    peerConnection.ontrack = (event) => {
+        event.streams[0].getTracks().forEach((track) => {
+        remoteStream.addTrack(track);
         });
     };
+}
 
-    peerConnection.onicecandidate = event => {
-        if (event.candidate) {
-            // Append candidate to the textarea for manual transfer
-            const candidatesTextarea = document.getElementById('ice-candidates');
-            candidatesTextarea.value += JSON.stringify(event.candidate) + '\n';
+let createOffer = async () => {
+
+
+    peerConnection.onicecandidate = async (event) => {
+        //Event that fires off when a new offer ICE candidate is created
+        if(event.candidate){
+            document.getElementById('offer-sdp').value = JSON.stringify(peerConnection.localDescription)
         }
     };
-};
 
-const createOffer = async () => {
     const offer = await peerConnection.createOffer();
     await peerConnection.setLocalDescription(offer);
-    document.getElementById('offer-sdp').value = JSON.stringify(offer);
-};
+}
 
-const createAnswer = async () => {
-    const offer = JSON.parse(document.getElementById('offer-sdp').value);
-    await peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
-    const answer = await peerConnection.createAnswer();
-    await peerConnection.setLocalDescription(answer);
-    document.getElementById('answer-sdp').value = JSON.stringify(answer);
-};
+let createAnswer = async () => {
 
-const addAnswer = async () => {
-    const answer = JSON.parse(document.getElementById('answer-sdp').value);
-    await peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
-};
+    let offer = JSON.parse(document.getElementById('offer-sdp').value)
 
-const addIceCandidate = async () => {
-    const candidatesTextarea = document.getElementById('ice-candidates').value.trim().split('\n');
-    for (const candidateStr of candidatesTextarea) {
-        if (candidateStr) {
-            const candidate = JSON.parse(candidateStr);
-            await peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
+    peerConnection.onicecandidate = async (event) => {
+        //Event that fires off when a new answer ICE candidate is created
+        if(event.candidate){
+            console.log('Adding answer candidate...:', event.candidate)
+            document.getElementById('answer-sdp').value = JSON.stringify(peerConnection.localDescription)
         }
+    };
+
+    await peerConnection.setRemoteDescription(offer);
+
+    let answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(answer); 
+}
+
+let addAnswer = async () => {
+    console.log('Add answer triggerd')
+    let answer = JSON.parse(document.getElementById('answer-sdp').value)
+    console.log('answer:', answer)
+    if (!peerConnection.currentRemoteDescription){
+        peerConnection.setRemoteDescription(answer);
     }
-};
+}
 
-init();
+init()
 
-document.getElementById('create-offer').addEventListener('click', createOffer);
-document.getElementById('create-answer').addEventListener('click', createAnswer);
-document.getElementById('add-answer').addEventListener('click', addAnswer);
-document.getElementById('add-ice-candidate').addEventListener('click', addIceCandidate);
+document.getElementById('create-offer').addEventListener('click', createOffer)
+document.getElementById('create-answer').addEventListener('click', createAnswer)
+document.getElementById('add-answer').addEventListener('click', addAnswer)
